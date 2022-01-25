4e5cc26fae3293b26bbd7cdba5d7625e5a2f6f80
SANSKRITI JOSHI <84018422+Work28sanskriti@users.noreply.github.com>
2022-01-25 11:15:47 +0530

    Add files via upload

---
+++ carousel/Owl/index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Page Title</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <link href="/your-path-to-fontawesome/css/fontawesome.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/brands.css" rel="stylesheet">
+    <link href="/your-path-to-fontawesome/css/solid.css" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="css/owl.carousel.min.css">
+</head>
+
+<body>
+
+    <!--Testimonial-->
+
+
+    <section id="slider" class="pt-5">
+  <div class="container">
+    <h1 class="text-center"><b>Responsive Owl Carousel</b></h1>
+      <div class="slider">
+                <div class="owl-carousel">
+                    <!-- card1 -->
+                    <div class="slider-card">
+                        <div class="d-flex justify-content-center align-items-center mb-4">
+                            <img src="l3.png" alt="abc" >
+                        </div>
+                        <h5 class="mb-0 text-center"><b>HTML CSS3 Tutorials</b></h5>
+                        <p class="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                    </div>
+                    <!-- c2 -->
+                    <div class="slider-card">
+                        <div class="d-flex justify-content-center align-items-center mb-4">
+                        <img src="l3.png" alt="abc" >
+                        </div>
+                        <h5 class="mb-0 text-center"><b>Wordpress Tutorials</b></h5>
+                        <p class="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                    </div>
+                    <!-- c4 -->
+                    <div class="slider-card">
+                        <div class="d-flex justify-content-center align-items-center mb-4">
+                        <img src="l3.png" alt="abc" >
+                        </div>
+                        <h5 class="mb-0 text-center"><b>PHP MySQL Tutorials</b></h5>
+                        <p class="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                    </div>
+                    <!-- c5 -->
+                    <div class="slider-card">
+                        <div class="d-flex justify-content-center align-items-center mb-4">
+                        <img src="l3.png" alt="abc" >
+                        </div>
+                        <h5 class="mb-0 text-center"><b>Javascript Tutorials</b></h5>
+                        <p class="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                    </div>
+                    <!-- c6 -->
+                    <div class="slider-card">
+                        <div class="d-flex justify-content-center align-items-center mb-4">
+                        <img src="l3.png" alt="abc" >
+                        </div>
+                        <h5 class="mb-0 text-center"><b>Bootstrap Tutorials</b></h5>
+                        <p class="text-center p-4">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsam temporibus quidem magni qui doloribus quasi natus inventore nisi velit minima.</p>
+                    </div>
+                </div>
+            </div>
+  </div>
+</section>
+
+    <script src="js/owl.carousel.min.js"></script>
+
+    <!-- Bootstrap Setup -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    </script>
+<!-- -------End Bootstrap Setup-------------------------------------------------------------------------------- -->
+    <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/script.js"></script>
+
+</body>
+
+</html>
